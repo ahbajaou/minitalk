52e40b6641cdb5f9663c948e633bc2049a61ffea
Ahmed Bajaou <ahbajaou@e2r6p7.1337.ma>
2023-01-31 04:26:51 +0100

    minitalk

--- client.c
+++ client.c
@@ -6,11 +6,17 @@
 /*   By: ahbajaou <ahbajaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/21 22:27:31 by ahbajaou          #+#    #+#             */
-/*   Updated: 2023/01/30 03:59:44 by ahbajaou         ###   ########.fr       */
+/*   Updated: 2023/01/30 23:36:48 by ahbajaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+void	check_error(void)
+{
+	write(1, "---PID INVALID---\n", 19);
+	exit(0);
+}
 
 int	ft_atoi(char	*str)
 {
@@ -29,15 +35,17 @@ int	ft_atoi(char	*str)
 			k *= -1;
 		i++;
 	}
-	while (str[i] >= '0' && str[i] <= '9')
+	if (str[i] >= '0' && str[i] <= '9')
 	{
-		j = (j * 10) + str[i] - 48;
-		i++;
+		while (str[i] >= '0' && str[i] <= '9')
+			j = (j * 10) + str[i++] - 48;
 	}
+	else
+		check_error();
 	return (k * j);
 }
 
-void	ft_send_msg(int pid, char *msg)
+void	ft_send_msg(int pid, unsigned char *msg)
 {
 	int	i;
 
@@ -48,15 +56,16 @@ void	ft_send_msg(int pid, char *msg)
 			kill(pid, SIGUSR1);
 		else
 			kill(pid, SIGUSR2);
-		usleep(200);
+		usleep(400);
 		i++;
 	}
+	free(msg);
 }
 
-char	*to_binary(char msg)
+unsigned char	*to_binary(unsigned char msg)
 {
-	int		i;
-	char	*new;
+	int				i;
+	unsigned char	*new;
 
 	i = 7;
 	new = malloc(9);
@@ -72,23 +81,24 @@ char	*to_binary(char msg)
 
 int	main(int ac, char **av)
 {
-	int		pid;
-	int		i;
-	char	*msg;
+	int				pid;
+	int				i;
+	unsigned char	*msg;
 
 	i = 0;
 	if (ac == 3)
 	{
 		pid = ft_atoi(av[1]);
+		if (pid == -1 || pid == 0)
+			return (1);
 		while (av[2][i])
 		{
-			msg = to_binary(av[2][i]);
+			msg = (unsigned char *)to_binary(av[2][i]);
 			ft_send_msg(pid, msg);
 			i++;
 		}
-		free(msg);
 	}
 	else
-		write(1, "Rah Khass Ekono 3 dl'param\n", 28);
+		write(1, "---Please enter three argument---\n", 35);
 	return (0);
 }
